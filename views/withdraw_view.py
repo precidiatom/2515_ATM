af1c27305b020ab80cd4542846817ffaefbf61db
emilieez <emilie.m.zhang@gmail.com>
2018-04-13 16:30:51 -0700

    Withdraw only show available account buttons; show balance in withdraw

--- views/withdraw_view.py
+++ views/withdraw_view.py
@@ -13,18 +13,28 @@ class ViewWithdraw():
         self.welcome = tk.Label(self.overall_frame, text='Withdraw From Which Account?', font=COMICFONT)
         self.welcome.grid(row=0, column=0)
 
+        self.current_balance = tk.Label(self.overall_frame)
+
         self.chequing_but = tk.Button(self.overall_frame, text="Chequing", font=COMICSMALL)
-        self.chequing_but.grid(row=1, column=0)
         self.chequing_value = tk.Label(self.overall_frame)
-        self.chequing_value.grid(row=2, column=0)
 
         self.savings_but = tk.Button(self.overall_frame, text="Savings", font=COMICSMALL)
-        self.savings_but.grid(row=3, column=0)
         self.savings_value = tk.Label(self.overall_frame)
-        self.savings_value.grid(row=4, column=0)
 
         self.main_menu_btn = tk.Button(self.overall_frame, text="Return to Main Menu", font=COMICSMALL)
         self.main_menu_btn.grid(row=7, column=0)
+
+    def show_chequing(self):
+        self.chequing_but.grid(row=2, column=0)
+        self.chequing_value.grid(row=3, column=0)
+
+    def show_saving(self):
+        self.savings_but.grid(row=4, column=0)
+        self.savings_value.grid(row=5, column=0)
+
+    def show_current_balance(self, account_type, balance):
+        self.current_balance.grid(row=1, column=0)
+        self.current_balance.config(text='{} balance: {}'.format(account_type, balance))
 
     def remove_main_frame(self):
         for widgets in self.root.winfo_children():
