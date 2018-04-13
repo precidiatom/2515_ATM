e7fe5990779e78cc1e4e16fa0792060e958aef52
emilieez <emilie.m.zhang@gmail.com>
2018-04-13 14:44:03 -0700

    Show balance on show balance

--- controllers/view_balance_controller.py
+++ controllers/view_balance_controller.py
@@ -1,14 +1,18 @@
-from views.balance_view import ViewBalance
+from controllers.child_controller import ChildController
+from models.user import User
 from views.balance_view import ViewBalance
 
 
-class ViewBalanceController():
+class ViewBalanceController(ChildController):
     def __init__(self, frame_controller, user_id):
-        self.user = User.get_persist_user_obj(user_id)
+        super().__init__(frame_controller)
+        self.user = User(user_id)
         self.frame_controller = frame_controller
         self.interface = ViewBalance(self.frame_controller.master)
+        self.view = None
 
-        self.interface.show_balance(self.frame_controller.user_id)
+        for acc in self.user.accounts.keys():
+            self.interface.show_balance(self.user.accounts[acc]['balance'], account_type=acc)
         self.interface.main_menu_btn.config(
             command=lambda: self.frame_controller.change_controller('main_menu', self.frame_controller.user_id))
 
--- views/balance_view.py
+++ views/balance_view.py
@@ -18,8 +18,11 @@ class ViewBalance():
         self.acc_balance = tk.Label(self.overall_frame, text="Account Balance: ", font=COMICSMALL)
         self.acc_balance.grid(row=1, column=0)
 
-        self.acc_amt = tk.Label(self.overall_frame)
-        self.acc_amt.grid(row=2, column=0)
+        self.chq_acc_amt = tk.Label(self.overall_frame)
+        self.chq_acc_amt.grid(row=2, column=0)
+
+        self.sav_acc_amt = tk.Label(self.overall_frame)
+        self.sav_acc_amt.grid(row=2, column=0)
 
         self.main_menu_btn = tk.Button(self.overall_frame, text="Return to Main Menu", font=COMICSMALL)
         self.main_menu_btn.grid(row=8, column=0)
@@ -27,9 +30,15 @@ class ViewBalance():
     def remove_main_frame(self):
         self.destroy()
         self.acc_balance.grid_remove()
-        self.acc_amt.grid_remove()
+        self.chq_acc_amt.grid_remove()
+        self.sav_acc_amt.grid_remove()
         self.welcome.grid_remove()
         self.main_menu_btn.grid_remove()
 
-    def show_balance(self, balance):
-        self.acc_amt.config(text=balance)
+    def show_balance(self, balance, account_type):
+        print(balance, account_type)
+        if account_type == 'chequing_account':
+            print('meow moew moew')
+            self.chq_acc_amt.config(text='Chequing Account: $' + str(balance))
+        elif account_type == 'saving_account':
+            self.sav_acc_amt.config(text='Saving Account: $' + str(balance))
