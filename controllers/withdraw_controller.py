aa261171bcaf1a250e2a08b4e7e7f8dc31061e2c
emilieez <emilie.m.zhang@gmail.com>
2018-04-13 16:03:06 -0700

    Finish withdraw

--- controllers/withdraw_controller.py
+++ controllers/withdraw_controller.py
@@ -15,34 +15,37 @@ class WithdrawController():
         self.interface.chequing_but.config(command=self._click_chequing)
         self.interface.savings_but.config(command=self._click_saving)
 
-        self.selected_account = None
-
     def _click_chequing(self):
         self.interface = ViewWithdrawOptions()
-        self.selected_account = 'chequing_account'
         self.interface.welcome_account.config(text="Chequing")
-        self.interface.minus_20.bind("<Button-1>",
-                                     lambda ev, account_type='chequing_account', amt=20: self._withdraw(ev,
-                                                                                                        account_type,
-                                                                                                        amt))
-        self.interface.minus_40.config()
-        self.interface.minus_60.config()
-        self.interface.minus_80.config()
-        self.interface.minus_100.config()
+        self.interface.minus_20.config(
+            command=lambda account_type='chequing_account', amt=20: self._withdraw(account_type, amt))
+        self.interface.minus_40.config(
+            command=lambda account_type='chequing_account', amt=40: self._withdraw(account_type, amt))
+        self.interface.minus_60.config(
+            command=lambda account_type='chequing_account', amt=60: self._withdraw(account_type, amt))
+        self.interface.minus_80.config(
+            command=lambda account_type='chequing_account', amt=80: self._withdraw(account_type, amt))
+        self.interface.minus_100.config(
+            command=lambda account_type='chequing_account', amt=100: self._withdraw(account_type, amt))
         self.interface.mainmenu.config(command=lambda: self.interface.window.destroy())
 
     def _click_saving(self):
         self.interface = ViewWithdrawOptions()
-        self.selected_account = 'saving_account'
         self.interface.welcome_account.config(text="Savings")
-        self.interface.minus_20.config(command=self._withdraw('saving_account', 20))
-        self.interface.minus_40.config()
-        self.interface.minus_60.config()
-        self.interface.minus_80.config()
-        self.interface.minus_100.config()
+        self.interface.minus_20.config(
+            command=lambda account_type='saving_account', amt=20: self._withdraw(account_type, amt))
+        self.interface.minus_40.config(
+            command=lambda account_type='saving_account', amt=40: self._withdraw(account_type, amt))
+        self.interface.minus_60.config(
+            command=lambda account_type='saving_account', amt=60: self._withdraw(account_type, amt))
+        self.interface.minus_80.config(
+            command=lambda account_type='saving_account', amt=80: self._withdraw(account_type, amt))
+        self.interface.minus_100.config(
+            command=lambda account_type='saving_account', amt=100: self._withdraw(account_type, amt))
         self.interface.mainmenu.config(command=lambda: self.interface.window.destroy())
 
-    def _withdraw(self, ev, account_type, amount):
+    def _withdraw(self, account_type, amount):
         account = Account(self.user_id, account_type=account_type)
         account.withdraw(amount)
         print(account.balance, account.user.accounts[account_type]['balance'])
