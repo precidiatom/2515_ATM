5cfb5ad1cf0cec287531669309306e92142794d7
Precidia <precidia.tom@gmail.com>
2018-04-14 00:11:00 -0700

    DepositView's balances showing appropriately now

--- views/deposit_entry_frame.py
+++ views/deposit_entry_frame.py
@@ -5,32 +5,38 @@ COMICSMALL = "Comic Sans MS", 10
 
 
 class ViewDepositInput():
-    def __init__(self):
-        self.window = tk.Toplevel()
+    def __init__(self, master):
+        self.overall_frame = master
+        self.overall_frame = tk.Frame(self.overall_frame, bg="yellow")
+        self.overall_frame.grid(row=0, column=0, padx=25, pady=75)
 
-        self.overall_frame = tk.Frame(self.window)
-        self.overall_frame.grid(row=0, column=0)
-        self.welcome = tk.Label(self.overall_frame, text='Depositing into', font=COMICFONT)
+        self.top_frame = tk.Frame(self.overall_frame, bg="white")
+        self.top_frame.grid(row=0, column=0, pady=15, padx=25)
+        self.welcome = tk.Label(self.top_frame, text='Depositing into', font=COMICFONT)
         self.welcome.grid(row=0, column=0)
-        self.welcome_account = tk.Label(self.overall_frame, font=COMICFONT)
+        self.welcome_account = tk.Label(self.top_frame, font=COMICFONT)
         self.welcome_account.grid(row=0,column=1)
 
-        self.display_balance = tk.Label(self.overall_frame, text="Your current balance is: ", font=COMICSMALL)
-        self.display_balance.grid(row=1, column=0)
-        self.current_balance = tk.Label(self.overall_frame, font=COMICSMALL)
-        self.current_balance.grid(row=1, column=1)
+        self.mid_frame = tk.Frame(self.overall_frame, bg="white")
+        self.mid_frame.grid(row=2, column=0, padx=80, pady=25)
+        self.display_balance = tk.Label(self.mid_frame, text="Your current balance is: ", font=COMICSMALL)
+        self.display_balance.grid(row=2, column=0)
+        self.current_balance = tk.Label(self.mid_frame, font=COMICSMALL)
+        self.current_balance.grid(row=2, column=1)
 
-        self.deposit_label = tk.Label(self.overall_frame, text="Amount to Deposit:", font=COMICSMALL)
-        self.deposit_label.grid(row=2, column=1)
-        self.deposit_amt = tk.Entry(self.overall_frame, width=15)
-        self.deposit_amt.grid(row=3, column=1)
+        self.deposit_label = tk.Label(self.mid_frame, text="Amount to Deposit:", font=COMICSMALL)
+        self.deposit_label.grid(row=3, column=1)
+        self.deposit_amt = tk.Entry(self.mid_frame, width=15)
+        self.deposit_amt.grid(row=4, column=1)
 
-        self.deposit_but = tk.Button(self.overall_frame, text="Deposit", font=COMICSMALL)
-        self.deposit_but.grid(row=4, column=1)
+        self.deposit_but = tk.Button(self.mid_frame, text="Deposit", font=COMICSMALL)
+        self.deposit_but.grid(row=5, column=1, padx=5, pady=5)
 
-        self.mainmenu = tk.Button(self.overall_frame, text="Cancel Transaction. \n Return to Main Menu",
+        self.mainmenu = tk.Button(self.overall_frame, text="Return to Main Menu",
                                   font=COMICSMALL)
-        self.mainmenu.grid(row=9, column=2)
+        self.mainmenu.grid(row=9, column=2, pady=10)
+
+
 
 if __name__ == "__main__":
     root = tk.Tk()
